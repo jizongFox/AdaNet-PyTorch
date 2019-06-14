fix kl loss

utils.py
@@ -27,9 +27,7 @@ def _disable_tracking_bn_stats(model):
 
 def _l2_normalize(d):
     d_reshaped = d.view(d.shape[0], -1, *(1 for _ in range(d.dim() - 2)))
-    d /= torch.norm(d_reshaped, dim=1, keepdim=True)  # + 1e-8
-    assert torch.allclose(d.view(d.shape[0], -1).norm(dim=1), torch.ones(d.shape[0]).to(d.device),
-                          rtol=1e-3), f"Normalization wrong"
+    d /= torch.norm(d_reshaped, dim=1, keepdim=True)
     return d
 
 